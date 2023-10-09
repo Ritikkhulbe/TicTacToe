0569b7eba8d7c99c63be3fa9059935762d62a051
Ritik <91615440+Ritikkhulbe@users.noreply.github.com>
2023-10-09 15:31:35 +0530

    Names
    Getting both player's names

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,8 +2,29 @@
 #include <vector>
 using namespace std;
 
+class t{
+    public:
+    string p1;
+    string p2;
+
+    void getnames(string name1, string name2){
+        this->p1 = name1;
+        this->p2 = name2;
+    }
+};
+
 int main(){
     bool play = true;
+    string p1,p2;
+    cout<<"Enter first player's name: ";
+    cin>>p1;
+    cout<<"Enter second player's name: ";
+    cin>>p2;
+
+    t obj;
+    obj.getnames(p1,p2);
+
+
     while(play){
         tictactoe();
         char ch;
